5e4d0336526068e7deb279eb240c765550257176
LuMiSxh <schmidt.lucamichael@icloud.com>
2023-03-22 21:11:31 +0100

    Removing the old api

--- src/routes/oapi/auth/bungie-login/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from '@sveltejs/kit';
-import { redirect } from '@sveltejs/kit';
-import { SECRET_CLIENT_ID } from '$env/static/private';
-
-export const GET = (async () => {
-	const url = `https://www.bungie.net/en/OAuth/Authorize?client_id=${SECRET_CLIENT_ID}&response_type=code`;
-	throw redirect(303, url);
-}) satisfies RequestHandler;
